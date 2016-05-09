7082dff568cf1ca82d7d0d2b7cfeb86cab01543b
Shirley Rozenboim <email": null>
2016-05-09 14:58:22 -0700

    Checkpoint challenge

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,5 +1,5 @@
 class SodaMachine
-  attr_reader :sodas, :cash
+  attr_accessor :sodas, :cash
 
   def initialize(args = {})
     @sodas = args[:sodas]
@@ -7,12 +7,28 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
+  #look for soda_brand in the sodas array, return the soda_brand if it's there
   def find_soda(soda_brand)
+    if @sodas.include?(soda_brand.downcase) == true
+      soda_brand
+    else
+      return nil
+    end
   end
+
 
   def sell(soda_brand)
+    if find_soda(soda_brand) == true
+      @cash += @sodas[soda_brand]
+      @sodas.delete(soda_brand)
+    else
+      return nil
+    end
+
   end
 
 end
+
